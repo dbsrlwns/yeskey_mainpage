0e0915aaa8fe0ef9b0a5a5d1f6a394613ac37288
dbsrlwns <rjswmr@naver.com>
2021-08-23 16:46:58 +0900

    수정

--- script/script.js
+++ script/script.js
@@ -2,6 +2,40 @@
 
 $(function () {
     
+    // 검색 영역
+    $(".searchArea>a").click(function (e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $(".searchBox").stop().slideToggle(300);
+    });
+    
+    
+    $(".searchInput").keyup(function (){
+        var txt = $(this).val();
+        if(txt == ""){
+            $(".hidden_text").css({"visibility":"hidden"});
+        } else {
+            $(".hidden_text").css({"visibility":"visible"});
+        }
+    });
+
+    $(".hidden_text").click(function (){
+        $(".searchInput").val("");
+        $(this).css({"visibility":"hidden"});
+        $(".searchInput").focus();
+    });
+    
+    $(".searchIcon").click(function (){
+        var txt = $(".searchInput").val();
+        if(txt == ""){
+            alert("검색어를 입력해주세요!");
+        } else {
+            alert("잘못된 검색어입니다.");
+            $(".searchInput").val("");
+        }
+        $(".searchInput").focus();
+    });
+
     // 헤더영역 배경
     $("#header").mouseover(function () {
         $(this).addClass('active');
@@ -55,23 +89,53 @@ $(function () {
     }
     setInterval(fnSlide2);
 
-    var s1 = $(".section1").offset().top;
-    var s2 = $(".section2").offset().top;
-    var s3 = $(".section3").offset().top;
-    var s4 = $(".section4").offset().top;
-    var s5 = $(".section5").offset().top;
-    var s6 = $(".section6").offset().top;
-    var s7 = $(".section7").offset().top;
-    var s8 = $(".section8").offset().top;
+
+    var executed = false;
 
     $(window).scroll(function (){
         var wScroll = $(this).scrollTop();
+        var s1 = $(".section1").offset().top;
+        var s2 = $(".section2").offset().top;
+        var s3 = $(".section3").offset().top;
+        var s4 = $(".section4").offset().top;
+        var s5 = $(".section5").offset().top;
+        var s6 = $(".section6").offset().top;
+        var s7 = $(".section7").offset().top;
+        var s8 = $(".section8").offset().top;
+        var rate1 = $(".number1").attr("data-rate");
+        var rate2 = $(".number2").attr("data-rate");
+        var rate3 = $(".number3").attr("data-rate");
+
         console.log(wScroll);
         if(wScroll > s1 - 400){
-            $(".section1").animate({"opacity":"1"},1000);
-            $(".iconBox:nth-child(1)").animate({"opacity":"1"},2000);
-            $(".iconBox:nth-child(2)").animate({"opacity":"1"},3000);
-            $(".iconBox:nth-child(3)").animate({"opacity":"1"},4000);
+                if(!executed){
+                $(".section1").animate({"opacity":"1"},1000);
+                $(".iconBox:nth-child(1)").animate({"opacity":"1"},2000);
+                $(".iconBox:nth-child(2)").animate({"opacity":"1"},3000);
+                $(".iconBox:nth-child(3)").animate({"opacity":"1"},4000);
+                $({percent:0}).animate({percent:rate1},{
+                        duration:3000,
+                        progress:function(){
+                            var now = this.percent;
+                            $(".number1").text(Math.ceil(now));
+                    }
+                });
+                $({percent:0}).animate({percent:rate2},{
+                    duration:3000,
+                    progress:function(){
+                        var now = this.percent;
+                        $(".number2").text(Math.ceil(now));
+                }   
+            });
+                $({percent:0}).animate({percent:rate3},{
+                    duration:3000,
+                    progress:function(){
+                        var now = this.percent;
+                        $(".number3").text(Math.ceil(now));
+                    }   
+                });
+            } 
+            executed = true;
         }
         if(wScroll > s2 - 400){
             $(".section2").animate({"opacity":"1"},1000);
@@ -102,10 +166,10 @@ $(function () {
         if(wScroll > s6 - 400){
             $(".section6").animate({"opacity":"1"},1000);
             $(".otp_bg").animate({"top":"-20px"},800);
-            $(".otp_icon1").animate({"top":"-60px"},1000);
-            $(".otp_icon2").animate({"top":"-60px"},1500);
-            $(".otp_icon3").animate({"top":"-60px"},2000);
-            $(".otp_icon4").animate({"top":"-60px"},2500);
+            $(".otp_icon1").animate({"top":"-80px"},1000);
+            $(".otp_icon2").animate({"top":"-80px"},1500);
+            $(".otp_icon3").animate({"top":"-80px"},2000);
+            $(".otp_icon4").animate({"top":"-80px"},2500);
             $(".otp_box").animate({"left":"0"},800);
         }
         if(wScroll > s7 - 400){
@@ -123,5 +187,4 @@ $(function () {
             $(".info_box:nth-child(3)").animate({"top":"0"},2000);
         }
     });
-
 });
